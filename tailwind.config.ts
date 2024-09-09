da5ad06d8130a22cb9aca11238a680c9098be808
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-09-09 18:13:04 +0300

    initial template

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}", "./plugins/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools"
+
+export default defineConfig({
+  plugins: [
+    remixDevTools({
+      pluginDir: "./plugins",
+    }),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  server : {
+    open: true,
+  },
+});
