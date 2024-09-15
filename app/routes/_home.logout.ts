fcb62607577794483906fd3f7c245aada89c72c2
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-09-15 23:27:20 +0300

    logout problem proposed fix

--- app/routes/_home.logout.ts
+++ app/routes/_home.logout.ts
@@ -1,12 +1,10 @@
 import { LoaderFunction, LoaderFunctionArgs, redirect } from '@remix-run/node';
-import { deleteUserMagicTokens } from '~/.server/models/token.model';
 import { requireUser } from '~/gaurds.server';
 import { destroyUserSession } from '~/sessions.server';
 
 export const loader: LoaderFunction = async ({ request }: LoaderFunctionArgs) => {
-  const user = await requireUser(request);
+  await requireUser(request);
   const sessionHeader = await destroyUserSession(request);
-  await deleteUserMagicTokens(user.id);
 
   return redirect('/feed', {
     headers: {
--- build.sh
+++ build.sh
@@ -6,16 +6,23 @@ NODE_PID=$(pgrep -f node)
 # Kill the node process
 if [ -n "$NODE_PID" ]; then
     echo "===> Killing existing node process with PID: $NODE_PID"
+    echo ""
     sudo kill $NODE_PID
 else
     echo "===> No node process found to kill."
+    echo ""
 fi
 
+echo ""
 echo "===> Removing old build..."
+echo ""
 rm -rf build/
 
+echo ""
 echo "===> Building new build..."
+echo ""
 npm run build
 
+echo ""
 echo "===> starting production server..."
 npm start > ../log/output.log 2>&1 &
